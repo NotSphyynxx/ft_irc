632988c6df24fa23e47c0b4423c1b2c8514b8785
Sphynx <thesphynx48@gmail.com>
2026-05-16 02:37:17 +0100

    done

--- server/Commands.cpp
+++ server/Commands.cpp
@@ -284,6 +284,9 @@ void Server::processCommand(std::string line, int sock)
                     chan = getChannel(singleChan);
                     chan->addOperator(&cl); // Grant the Crown
                 } else {
+					if (chan->isMember(&cl)) {
+                        continue; // if user is arleady on the channel
+                    }
                     chan->addMember(&cl);
                 }
 
